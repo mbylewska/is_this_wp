54381607c0e28565316886d1a414eae019a3ac55
Marta Bylewska <martabylewska@gmail.com>
2023-01-24 12:40:00 +0100

    a bit of styling

--- isitwp.php
+++ isitwp.php
@@ -12,17 +12,56 @@
             display: flex;
             justify-content: center;
             align-items: center;
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif
         }
 
-        .container {
+        /* .container {
             margin: auto;
             text-align: center;
 
+        } */
+
+        .container {
+            margin-top: 3rem;
+            position: relative;
+            text-align: center;
+            /* border-radius: 20px; */
+            padding: 50px;
+            box-sizing: border-box;
+            background: #E6EDF0;
+            box-shadow: 14px 14px 20px #D9E4E8, -14px -14px 20px white;
         }
 
         h2 {
             color: black;
             font-size: 3rem;
+            padding-bottom: 1rem;
+        }
+
+        input {
+            padding: 0.5rem;
+            border: none;
+            /* box-shadow: inset 6px 6px 6px #D9E4E8, inset -6px -6px 6px white; */
+        }
+
+        .btn {
+            /* box-shadow: 6px 6px 6px #cbced1, -6px -6px 6px white; */
+            transition: 0.5s;
+            background-color: #B3C9D1;
+        }
+
+        .btn:hover {
+            background-color: #8CAEBA;
+
+        }
+
+        p {
+            padding-top: 1rem;
+        }
+
+        .message {
+            padding-top: 1rem;
+            font-size: 2rem;
         }
     </style>
 </head>
@@ -33,7 +72,7 @@
 
         <form name='website' method="POST">
             <label for="url">Put URL here: </label><input type="text" name="url" placeholder="https://mysite.com">
-            <input type="submit" value="Submit">
+            <input class="btn" type="submit" value="Submit">
 
         </form>
         <?php
@@ -59,7 +98,7 @@
 
         ?>
 
-        <p><?php echo $message ?></p>
+        <p class="message"><?php echo $message ?></p>
 
     </div>
 </body>
